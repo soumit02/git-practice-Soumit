17b2dbc186a51f23799a700cd58a1542a7acbfaa
Soumit Dey <soumitdey2002@gmail.com>
2026-04-30 18:32:55 +0600

    Add division error handling

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import datetime
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply,divide
 
 print("Soumit Dey")
 print("Today's date is:", datetime.date.today())
 print(add(5,6))
 print(subtract(4,3))
 print(multiply(7,8))
+print(divide(0,0))
--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,11 @@ def subtract(a, b):
 
 def multiply(a, b):
     return a * b
+
+def divide(a,b):
+    try:
+        result=a/b
+        return result
+    except Exception as e:
+        print(f"Error: {e}")
+
